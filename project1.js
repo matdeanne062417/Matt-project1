d57fdfccc473172ac08c29cc18ae4ffc07cbc06d
matthew balagtas <matthew.balagtas007@gmail.com>
2020-09-09 23:47:41 +0800

    added input name display

--- project1.js
+++ project1.js
@@ -1,3 +1,21 @@
-function myFunction(){
-    document.getElementById('greeter-display').innerHTML = 'Hello World!';
-}
+const display = document.querySelector('#greeter-display');
+const greet_button = document.querySelector('.greeter-buttons');
+
+greet_button.addEventListener('click', function(e){
+    if (e.target.matches('button')){
+        const key = e.target;
+        
+        if (key.dataset.action === 'hello'){
+           display.textContent = 'Hello World!';
+        }
+    }
+
+    if (e.target.matches('input')){
+        const keys = e.target;
+        const myName = document.getElementById('fname');
+        if (keys.value === 'submit'){
+            display.textContent = myName.value;
+            }
+    }   
+})
+
